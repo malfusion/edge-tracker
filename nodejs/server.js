b63ddab50856fdbeaf10721fba9c807b3c2a1fc9
malfusion <judenaveenraj@gmail.com>
2021-12-11 16:33:59 -0800

    Changed host and port

--- nodejs/server.js
+++ nodejs/server.js
@@ -3,7 +3,7 @@ const redis = require('redis');
 
 // Configuration: adapt to your environment
 const REDIS_SERVER = "redis://localhost:6379/0";
-const WEB_SOCKET_PORT = 3000;
+const WEB_SOCKET_PORT = 5000;
 
 // Connect to Redis and subscribe to "app:notifications" channel
 var redisClient = redis.createClient(REDIS_SERVER);
